b8df8a4282cfc1854257f47d735d5cae5c36124a
alifa-ara-heya <alifaaraheya@gmail.com>
2025-10-16 19:06:07 -0400

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+     <h1>This is Parcel delivery frontend</h1>
+     <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
