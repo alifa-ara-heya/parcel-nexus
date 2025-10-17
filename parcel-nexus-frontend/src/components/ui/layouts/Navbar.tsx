c7b12363e8ada3df3e16bd80b4bfa48c8aa8aee5
alifa-ara-heya <alifaaraheya@gmail.com>
2025-10-17 19:05:34 -0400

    feat: implement theme toggler and dropdown menu components; update index and main files

--- parcel-nexus-frontend/src/components/ui/layouts/Navbar.tsx
+++ parcel-nexus-frontend/src/components/ui/layouts/Navbar.tsx
@@ -12,6 +12,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { Link } from "react-router"
+import { ModeToggle } from "./ModeToggler"
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
@@ -106,6 +107,7 @@ export default function Navbar() {
         </div>
         {/* Right side */}
         <div className="flex items-center gap-2">
+          <ModeToggle />
           <Button asChild variant="ghost" size="sm" className="text-sm">
             <a href="#">Sign In</a>
           </Button>
